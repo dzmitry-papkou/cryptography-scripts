47582fc035480a945f558723a063629b4e51c946
NorthDragonDuck <113013199+NorthDragonDuck@users.noreply.github.com>
2023-09-18 09:04:57 +0300

    Minor scripts improvements
    1. Remove useless comments
    2. Add more automation with bash

--- hill-cipher.py
+++ hill-cipher.py
@@ -1,12 +1,6 @@
+#!/usr/bin/env python3
+
 # Description: Hill cipher decryption script
-
-# Alphabet where:
-
-# 0  1  2  3  4  5  6  7  8  9  10 11 12
-# A  B  C  D  E  F  G  H  I  J  K  L  M
-
-# 13 14 15 16 17 18 29 20 21 22 23 24 25
-# N  O  P  Q  R  S  T  u  V  W  X  Y  Z
 
 import numpy as np
 
